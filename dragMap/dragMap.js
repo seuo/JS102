b66b83e17b86e86de229c7d22ded764387f1fd84
Sam Robertson <samrob.nz@gmail.com>
2019-05-21 15:21:04 +1200

    dragmap update

--- dragMap/dragMap.js
+++ dragMap/dragMap.js
@@ -18,54 +18,46 @@ container.addEventListener("mouseup", dragEnd, false);
 container.addEventListener("mousemove", drag, false);
 
 function dragStart(e) {
-    if (e.type ==="touchstart"){
-        initialX = e.clientX - xOffset;
-        initialY = e.ciientY - yOffset;
-    }
-    else {
-        initialX = e.clientX - xOffset;
-        initialY = e.ciientY - yOffset;
-    }
+  if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
 
-    if (e.target === dragItem) {
-        active = true;
-    }
+  if (e.target === dragItem) {
+    active = true;
+  }
 }
 
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
 
+  active = false;
+}
 
 function drag(e) {
-    if (active){
-        e.preventDefault();
-
-        if (e.type === "touchmove") {
-            currentX = e.clientX[0].clientX - xOffset;
-            currentY = e.clientY[0].clientY - yOffset;
-        } 
-
-        else{
-            currentX = e.clientX - xOffset;
-            currentY = e.clientY - yOffset;
-        }
-
-        xOffset = currentX;
-        yOffset = currentY;
-
-        setTranslate(currentX, currentY, dragItem);
+  if (active) {
+  
+    e.preventDefault();
+  
+    if (e.type === "touchmove") {
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+    } else {
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
     }
-    
-}
 
-function dragEnd(e){
-    initialX = currentX;
-    initialY = currentY;
+    xOffset = currentX;
+    yOffset = currentY;
 
-    active = false;
+    setTranslate(currentX, currentY, dragItem);
+  }
 }
 
 function setTranslate(xPos, yPos, el) {
-    el.style.transform = "translate3d("+ xPos +"px, " + yPos + "px, 0)";
-
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
-
-
